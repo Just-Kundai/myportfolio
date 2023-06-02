9aa546dd8920bd6bfaacc66e6b52ab140a5b42e5
Just-Kundai <kundaikhuleya@outlook.com>
2023-06-02 19:10:04 +1000

    new portfolio

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,1 +1,16 @@
-import React from React
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Projects from './Components/Projects';
+
+const App = () => {
+  return (
+    <Router>
+    <div>
+    <Switch>
+    <Route path="/projects">
+    <Projects />
+    </Switch>
+    </div>
+    </Router>
+  )
+}
