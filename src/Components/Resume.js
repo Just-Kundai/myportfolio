732f44cc050dc17783798b43eb0036a14433532b
Just-Kundai <kundaikhuleya@outlook.com>
2023-06-02 19:28:08 +1000

    resume component

---
+++ src/Components/Resume.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Resume = () => {
+  const resumeData = {
+    name: 'Kundai Khuleya',
+    title: 'Junior Frontend Developer',
+    contact: {
+      email: 'kundaikhuleya@outlook.com',
+
+    },
+    skills: ['HTML', 'CSS', 'JAVASCRIPT', 'REACT'],
+    experiences: [
+      {
+        id: 1,
+        title: 'Developer Intern',
+        company: 'IAG',
+        fromDate:'2022',
+        toDate: 'PRESENT'
+      },
+    ],
+  };
+
+  return (
+  <div>
+    <h2> Resume </h2>
+    <h3>{resumeData.name}</h3>
+    <p>{resumeData.title}</p>
+
+
+    <div>
+    <h4>Contact</h4>
+    <p>Email: {resumeData.contact.email}</p>
+    </div>
+
+    <div>
+    <h4>Skills</h4>
+    <ul>
+    {
+      resumeData.skills.map((skill, index) => (<li key={index}>{skill}</li>))
+    }
+    </ul>
+    </div>
+
+  )
+}
